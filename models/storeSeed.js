92534d2ce67ac83f775cd34196d050e431ac8398
Lehi Cano <onacihel@gmail.com>
2022-12-18 20:09:38 -0500

    wip

---
+++ models/storeSeed.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+      name: 'Beans',
+      description: 'A small pile of beans.',
+      img: 'https://imgur.com/LEHS8h3.png',
+      price: 5,
+      qty: 99
+    }, {
+      name: 'Bones',
+      description: "It's just a bag of bones. Use it as currency.",
+      img: 'https://imgur.com/dalOqwk.png',
+      price: 25,
+      qty: 0
+    }, {
+      name: 'Bins',
+      description: 'A stack of colorful bins for your beans and bones.',
+      img: 'https://imgur.com/ptWDPO1.png',
+      price: 7000,
+      qty: 1
+    }
+  ]
