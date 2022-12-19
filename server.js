27ac66471001a175b6d5e907d0da018f704570d3
Lehi Cano <onacihel@gmail.com>
2022-12-18 20:09:38 -0500

    wip

--- server.js
+++ server.js
@@ -4,89 +4,67 @@ const app = express()
 const Store = require('./models/store.js')
 require('dotenv').config()
 const PORT = process.env.PORT
+// const storeSeed = require('./models/storeSeed.js')
 
 // Dependenacies
 const mongoose = require('mongoose')
-const Item = require("./models/store.js")
 const methodOverride = require("method-override")
-const store = require('./models/store.js')
 
 // Middle-ware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"))
-
 
 // Database Hook Up
 mongoose.connect(process.env.DATABASE_URI)
 
 // DB success/fail and callback
 const db = mongoose.connection
-db.on("error", (err) => console.log(err.message + " is mongo not running?"))
+db.on("error", (err) => console.log('MongoDB as encountered an error: ' + err.message))
 db.on("connected", () => console.log(`Mongo connected at ${db.host} : ${db.port}`))
 db.on('disconnected', () => console.log('Mongo disconnected'))
 
-app.get('/store/seed', (req, res) => {
-    Item.create(       
-    [
-        {
-          name: 'Beans',
-          description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-          img: 'https://imgur.com/LEHS8h3.png',
-          price: 5,
-          qty: 99
-        }, {
-          name: 'Bones',
-          description: "It's just a bag of bones.",
-          img: 'https://imgur.com/dalOqwk.png',
-          price: 25,
-          qty: 0
-        }, {
-          name: 'Bins',
-          description: 'A stack of colorful bins for your beans and bones.',
-          img: 'https://imgur.com/ptWDPO1.png',
-          price: 7000,
-          qty: 1
-        }
-      ],
-      (error, data) => {
+// SEED
+app.get('/store/seed', (req,res) => {
+    Store.create(storeSeed, (error, item) => {
         res.redirect('/store')
-      }
-    )
-    })
+    }) 
+})
 
-// I 
+// INDEX
 app.get('/store', (req,res) => {
-    Store.find({}, (error, allItems) => {
-        res.render('index.ejs', { items: allItems})
+    Store.find({}, (error, items) => {
+        res.render('index.ejs', {items})
+        // console.log(items);
     })
 })
 
-// N 
+// NEW
+app.get('/store/new', (req,res) => {
+    res.render('new.ejs')
+})
 
-// D
+// DELETE
 app.delete('/store/:id', (req,res) =>{
     Store.findByIdAndDelete(req.params.id, (err, data) =>{
         res.redirect('/store')
     })
 })
 
-// U 
-// C : Create
+// UPDATE
+// CREATE
 app.post('/store', (req,res) => {
     Store.create(req.body, (err, item) => {
-        res.send(item)
+        res.redirect('/store')
     })
 })
-// E
+// EDIT
 // app.get('/store/:id/edit', )
 
-// // S
-// app.get('/store/:id', (req,res) => {
-//     foundItem.findById(req.params.id, (err, foundItem) => {
-//         res.render('show.ejs', {
-//             item: foundItem
-//         })
-//     })
-// })
+// SHOW
+app.get('/store/:id', (req,res) => {
+    Store.findById(req.params.id, (err, item) => {
+        res.render('show.ejs', {item})
+    })
+})
 
 app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`))
